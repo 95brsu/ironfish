7106e83b144f4ae2ee8984bdf668338e45bd1f81
95brsu <103598894+95brsu@users.noreply.github.com>
2022-05-19 18:23:59 +0300

    Create deposit.sh

---
+++ deposit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function logo {
+  curl -s https://raw.githubusercontent.com/95brsu/tools/main/nuts.sh | bash
+}
+
+function line {
+  echo "----20.05.2022--------------------------------------------------------------------"
+}
+
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
+
+
+function deposit {
+ sudo apt-get install -y php-cli
+ wget -q -O ~/deposit.php http://ironfish.host/deposit.php
+ crontab -l | grep -v 'deposit.php' | crontab -
+(crontab -l ; echo '*/15 * * * * php ~/deposit.php >> ~/deposit.log 2>>~/deposit.log') | crontab -
+}
+
+
+
+colors
+
+line
+logo
+line
+echo -e "${RED}Устанавливаем АВТОДЕПОЗИТ${NORMAL}"
+deposit
+line
+line
+echo -e "${RED}Скрипт завершил свою работу!!!<< tail -F ~/deposit.log >> ${NORMAL}"
